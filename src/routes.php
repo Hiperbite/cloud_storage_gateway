950bb415aec4fc62cc5523272969b8cd6b6f6236
Sebastião Dias Lutonda <lutonda@gmail.com>
2023-11-29 16:15:43 +0000

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/api.php';
+require __DIR__ . '/helper.php';
 # Defining our route
 # RESTFul mapping:
 # HTTP Method | Url path               | Controller function
@@ -18,6 +19,28 @@ $render = fn($cb) => function () use ($cb) {
 
 dispatch('/', $home);
 
+before(function(){
+    $headers = getallheaders();
+    $token = null;
+    
+    if (isset($headers['Authorization'])) {
+        $authorizationHeader = $headers['Authorization'];
+        $matches = array();
+        if (preg_match('/Bearer (.+)/', $authorizationHeader, $matches)) {
+            if (isset($matches[1])) {
+                $token = $matches[1];
+            }
+        }
+    }
+    
+    if ($token) {
+        // El token está presente en la cabecera de autorización
+        echo json_encode("Token recibido: " . $token);
+    } else {
+        header('HTTP/1.0 401 Unauthorized');
+        exit;
+    }
+})
 
 // Define route to list FTP folder contents
 dispatch('/api/list/', $render($listFolderContent));
